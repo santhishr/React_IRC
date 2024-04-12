70c3d534af0f26f4583f39aa1c8758682bb0b232
santhishr <santhish4002@gmail.com>
2024-04-12 11:38:32 +0530

    bb

---
+++ backend/src/main/java/com/freshnco/backend/model/User.java
@@ -0,0 +1,77 @@
+package com.freshnco.backend.model;
+
+
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "user")
+public class User {
+	@Id
+@GeneratedValue
+private Long id;
+private String fname;
+private String lname;
+private String email;
+private String password;
+@OneToOne(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Details details;
+
+    // Constructors, getters, and setters
+
+    public Details getDetails() {
+        return details;
+    }
+
+    public void setDetails(Details details) {
+        this.details = details;
+    }
+public User(){
+
+}
+
+public Long getId() {
+	return id;
+}public User(Long id, String fname, String lname, String email, String password) {
+	this.id = id;
+	this.fname = fname;
+	this.lname = lname;
+	this.email = email;
+	this.password = password;
+}
+
+public void setId(Long id) {
+	this.id = id;
+}
+public String getFname() {
+	return fname;
+}
+public void setFname(String fname) {
+	this.fname = fname;
+}
+public String getLname() {
+	return lname;
+}
+public void setLname(String lname) {
+	this.lname = lname;
+}
+public String getEmail() {
+	return email;
+}
+public void setEmail(String email) {
+	this.email = email;
+}
+public String getPassword() {
+	return password;
+}
+public void setPassword(String password) {
+	this.password = password;
+}
+
+	
+}
